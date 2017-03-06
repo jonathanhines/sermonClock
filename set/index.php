42b812281e68b7823e1f062394179b2282f0a2ef
Jonathan Hines <jon_hines@hotmail.com>
2017-03-05 23:15:06 -0600

    Fixed a broken style reference and added a success message.  Also added a sample htaccess file with httpauth for the admin file.

--- set/index.php
+++ set/index.php
@@ -2,9 +2,11 @@
   date_default_timezone_set ("America/Toronto");
   $timeFilepath = '../data/time.txt';
   $timeTarget = false;
+  $successMessage = false;
   if(isset($_POST['targetTime'])) {
     $timeTarget = $_POST['targetTime'];
     file_put_contents($timeFilepath, $timeTarget);
+    $successMessage = "Timer was updated, now set to $timeTarget";
   } elseif(file_exists($timeFilepath)) {
     $timeTarget = file_get_contents($timeFilepath);
   }
@@ -17,7 +19,7 @@
   <title></title>
   <meta name="description" content="Sermon Clock">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="/css/style.css">
   <link href="https://fonts.googleapis.com/css?family=Cabin|Oswald" rel="stylesheet">
 </head>
 
@@ -26,8 +28,14 @@
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
   <div class='container'>
-    <h1>Sermon Timer</h1>
-    <form method="post" target="">
+    <h1>Sermon Timer</h1><?php
+      if( $successMessage ) {
+        ?><div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  <strong>Success!</strong> <?php echo $successMessage; ?>
+</div><?php
+      }
+    ?><form method="post" target="">
       <div class="form-group">
         <label for="targetTime">TargetTime</label>
         <input type="text" class="form-control" id="targetTime" placeholder="11:55am" name="targetTime"<?php
@@ -40,12 +48,12 @@
   </div>
   <script src="/bower_components/jquery/dist/jquery.min.js"></script>
   <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-  <script src="/bower_components/jquery-textfill/source/jquery.textfill.min.js"></script>
+  <!--script src="/bower_components/jquery-textfill/source/jquery.textfill.min.js"></script>
   <script>
     __reloadServerUrl="ws://localhost:6001";
   </script>
   <script type="text/javascript" src="//localhost:6001/__reload/client.js"></script>
-  <script type="text/javascript" src="/js/set.js"></script>
+  <script type="text/javascript" src="/js/set.js"></script-->
 </body>
 
 </html>
