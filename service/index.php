9163226a049deb88b31ee566aeab788b92802258
Jonathan Hines <jonathan.hines@gmail.com>
2019-01-02 16:30:54 -0500

    Updated the error alert html element to use the right class.

--- service/index.php
+++ service/index.php
@@ -96,7 +96,7 @@ if($successMessage) {
     </div><?php
 }
 if($errorMessage) {
-  ?><div class="alert alert-error alert-dismissible" role="alert">
+  ?><div class="alert alert-danger alert-dismissible" role="alert">
     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
     <strong>Error: </strong> <?php echo $errorMessage; ?>
     </div><?php
--- set/index.php
+++ set/index.php
@@ -112,7 +112,7 @@
 </div><?php
       }
       foreach($errorMessages as $message) {
-        ?><div class="alert alert-error alert-dismissible" role="alert">
+        ?><div class="alert alert-danger alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
   <strong>Error:</strong> <?php echo $message; ?>
 </div><?php
